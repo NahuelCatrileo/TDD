186c2f737ad153e4f21504d924f6c01723335743
NahuelCatrileo <n.catrileo02@ufromail.cl>
2024-11-15 11:27:26 -0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+/**
+ * com.example.dev Paquete de desarrollo de la solución
+ */
+package org.example;
+// -encoding UTF-8 -charset UTF-8 -docencoding UTF-8
+
+
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Clase Main");
+
+    }
+
+//aquí van sus funciones:
+
+}
